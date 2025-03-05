a05012d45da02028d0f794ca343cd927dc37b9f5
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-03-05 11:08:00 +0300

    feat: complete fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -226,8 +226,15 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const object = Object.create(proto);
+  const parsedJSON = JSON.parse(json);
+
+  Object.keys(parsedJSON).forEach((key) => {
+    object[key] = parsedJSON[key];
+  });
+
+  return object;
 }
 
 /**
